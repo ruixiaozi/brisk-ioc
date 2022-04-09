ffef8217ade37de12be2391a1d73948b168de73f
ruixiaozi <admin@ruixiaozi.com>
2022-04-10 02:32:23 +0800

    fix(3.0.6): 修改example和日志

--- example/src/index.ts
+++ example/src/index.ts
@@ -2,10 +2,11 @@ import { BriskIoC } from 'brisk-ioc';
 import { T2 } from './service/T2';
 
 (async function() {
-  await BriskIoC.core
+  await BriskIoC
     .configurate({
       isDebug: true,
     })
+    .core
     .scanPackage(__dirname,"./bean","./service")
     .initAsync();
 
