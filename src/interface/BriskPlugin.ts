41a7359af0e89357ee8ecf1294d66b04fbaf1f30
ruixiaozi <admin@ruixiaozi.com>
2022-05-01 17:11:40 +0800

    feat(3.0.7): 发布3.0.7

--- src/interface/BriskPlugin.ts
+++ src/interface/BriskPlugin.ts
@@ -6,7 +6,7 @@ import { Core } from '@core';
  * @description 插件接口
  * @author ruixiaozi
  * @email admin@ruixiaozi.com
- * @deprecated 弃用3.0.7, {@link register}
+ * @deprecated 弃用3.0.7, 请使用 {@link BriskPluginInterFace}
  */
 export interface BriskPlugin {
   name?: string;
